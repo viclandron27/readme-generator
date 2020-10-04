a7d8b1041689875c4168c88c628e4a16e7a930c8
Victoria Landron <viclandron@gmail.com>
2020-10-04 15:14:45 -0400

    moved top badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,8 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  # ${data.name} ![badmath](https://img.shields.io/static/v1?label=${data.license}&message=100%&color=<lightgrey>)
+  # ${data.name} 
+  ![badmath](https://img.shields.io/static/v1?label=${data.license}&message=100%&color=<lightgrey>)
 
 
   ## Description
