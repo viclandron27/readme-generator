1e97d53c6e5a2b7410f62152ca21cafeac9788b2
Victoria Landron <viclandron@gmail.com>
2020-10-04 11:20:25 -0400

    added questions for user

--- index.js
+++ index.js
@@ -1,7 +1,122 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+console.log(inquirer);
 
-];
+//const fs = require('fs');
+
+// array of questions for user
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project? (Required)',
+            validate: projectInput => {
+                if (projectInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter the name of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description', 
+            message: 'Provide a description of your project. (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter the description of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide the instructions for installation of your project. (Required)',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter the instructions for installation of your project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide the instructions for usage of your project. (Required)',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter the instructions for usage of your project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Choose a license for your project (Check the one that applies)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type:'input',
+            name: 'contributing',
+            message: 'Provide the guidelines for contributing to your project. (Required)',
+            validate: contributionInput => {
+                if (contributionInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please provide the guidelines for contributing to your project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Write tests for your application'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your Github username.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address. (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your email address.');
+                    return false;
+                }
+        }
+
+    ])
+} 
 
 // function to write README file
 function writeToFile(fileName, data) {
